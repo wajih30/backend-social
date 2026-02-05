e9b519eeac2f30d775704078d383ddf19711e78d
wajih30 <wajihurrehman2001@gmail.com>
2026-02-05 20:26:38 +0500

    Updated init file to import all the orm models so alembic can run migrations and changed the base class declaration to a simpler one because the advanced one was not needed

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,12 +1,5 @@
-from typing import Any
-from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy.ext.declarative import declarative_base
 
-@as_declarative()
-class Base:
-    id: Any
-    __name__: str
-    
-    # Generate __tablename__ automatically
-    @declared_attr
-    def __tablename__(cls) -> str:
-        return cls.__name__.lower()
+# The simplest, most standard way to create the Base class in SQLAlchemy.
+# This allows all models to inherit from a single foundation.
+Base = declarative_base()
